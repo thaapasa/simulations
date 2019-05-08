1db8f6dc9a435509967ced111c681aba3765028b
Tuukka Haapasalo <tuukka.haapasalo@reaktor.com>
2019-05-08 21:43:17 +0300

    Why not svg

--- src/ui/SimulationView.tsx
+++ src/ui/SimulationView.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import blackTile from '../icons/black-tile.png';
+import blackTile from '../icons/black-tile.svg';
 import grid from '../icons/grid.svg';
-import whiteTile from '../icons/white-tile.png';
+import whiteTile from '../icons/white-tile.svg';
 import { Colors } from './Colors';
 
 const tiles = {
