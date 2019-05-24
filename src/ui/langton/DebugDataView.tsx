41947f850e3096c729a7a546204798cdb3f9cf62
Tuukka Haapasalo <tuukka.haapasalo@gmail.com>
2019-05-24 22:07:44 +0300

    Fix debug deta view to be observer

--- src/ui/langton/DebugDataView.tsx
+++ src/ui/langton/DebugDataView.tsx
@@ -1,16 +1,17 @@
+import { observer } from 'mobx-react';
 import React from 'react';
 import styled from 'styled-components';
 import { Colors } from '../Colors';
 import { LangtonModel } from './LangtonModel';
 
-export const DebugDataView = ({ model }: { model: LangtonModel }) => (
+export const DebugDataView = observer(({ model }: { model: LangtonModel }) => (
   <DebugData>
     {dump('Center', model.centerPoint)}
     {dump('Render area', model.tileCalc.renderArea)}
     {dump('Tile range', model.tileCalc.tileRange)}
     {dump('Grid offset', model.tileCalc.gridOffset)}
   </DebugData>
-);
+));
 
 const dump = (title: string, data: any) => (
   <div>
